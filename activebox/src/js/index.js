833975eb3cc898ef3d8e669cfd1115a891f488a5
Your Name <pokemoojoo@gmail.com>
2022-09-15 22:26:20 +0300

    hometask

---
+++ activebox/src/js/index.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper(".swiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
+  
